fed8fcc3f3e540a2568b4da5be8d7ef8ad4e8459
ifratmitul <ifrat.mitul@hotmail.com>
2021-03-14 03:40:20 +0600

    adding pagination and other sorting option in backend

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,12 +3,42 @@ const Tour = require('./../models/tourModels');
 exports.getAllTours = async (req, res) => {
    
    try {
+
+    //filtering
     const queryObj = {...req.query}
     const excludedFields = ['page','sort','limit','fields'];
     excludedFields.forEach( el => delete queryObj[el]);
-   // console.log(req.query, queryObj)
+    console.log(req.query)
+    
+    //Advance filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+    console.log(JSON.parse(queryStr));
 
-    const query= await Tour.find(queryObj);
+    let query= Tour.find(JSON.parse(queryStr));
+
+    //sorting
+    //query = req.query.sort ? query.sort(req.query.sort.replace(/,/g, ' ')) : query.sort('-_id');
+    if(req.query.sort){
+
+        const sortBy = req.query.sort.split(',').join(' ');
+        console.log(sortBy);
+        query = query.sort(sortBy);
+    }
+    else{
+        query = query.sort('-createdAt');
+    }
+
+    //Field Limiting 
+    if(req.query.fields){
+        const fields = req.query.fields.split(',').join(' ');;
+        query = query.select(fields)
+    }
+    else{
+
+        query = query.select('-__v');
+    }
+
 
     const tours = await query;
     res.status(200).json({
